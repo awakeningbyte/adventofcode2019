8a017db1f78ef3810aa7c8126732ebfcac0a8826
George Chen <GChen@rapidrtc.com>
2020-01-08 16:40:04 -0600

    update day3.go

--- 2019/src/day3/day3.go
+++ 2019/src/day3/day3.go
@@ -6,6 +6,7 @@ import (
     "os"
     "strings"
     "regexp"
+    "fmt"
 )
 
 type Result struct {
@@ -19,7 +20,7 @@ func main() {
         fmt.Errorf("can not open input file %s", os.Args[1])
     }
 
-    result := Result {0, nil}
+    result := Result{0, Point{0,0}}
     var panel Panel 
     panel = *panel.Create()
     wires := strings.Split(text, "\n")
@@ -32,22 +33,22 @@ func main() {
         for _, p := range points {
             panel.Add(p)
         }
-        start = points[len(points]-1]
+        start = points[len(points)-1]
     }
 
     for _, line :=  range regx.FindAllString(wires[1]) {
         points := start.Go(line)
         for _, p := range points {
-            f : = panel.Add(p)
-            if f {
+            f := panel.Add(p)
+            if f== true {
                 dist := p.Distance(start)
-                if rsult.CrossPoint = nil || result.Distance > dist {
-                    result = Result {dist, p}
+                if result.CrossPoint == Point{0,0} || result.Distance > dist {
+                    result = Result{dist, p}
                 } 
             }
         }
-        start = points[len(points]-1]
+        start = points[len(points)-1]
     }
 
-    fmt.Printf("closest cross point is %v", result)
+    fmt.Printf("closest cross point is %v\n", result)
 }
